64f9d9abe9bbdf109cf032c59f109d894ef21720
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-12-18 13:02:57 +0800

    Update keys_and_addresses.py

--- keys_and_addresses.py
+++ keys_and_addresses.py
@@ -1,18 +1,12 @@
 # keys_and_addresses.py
 private_keys = [
-    'YOUR PRIVATE KEY WALLET 1',
-    'YOUR PRIVATE KEY WALLET 2',
-    'YOUR PRIVATE KEY WALLET 3'
+    'YOUR PRIVATE KEY WALLET 1'
 ]
 
 my_addresses = [
-    'ADDRESS 1',  # Alamat address EVM / Your EVM Address
-    'ADDRESS 2',
-    'ADDRESS 3'
+    'ADDRESS 1'
 ]
 
 labels = [
-    'Wallet 1',
-    'Wallet 2',
-    'Wallet 3'
+    'Wallet 1'
 ]
