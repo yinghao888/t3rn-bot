ac139043e3dd3576a5809b86590762eaeb1bcd56
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-12-18 13:43:22 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,12 +2,12 @@
 
 networks = {
     'Arbitrum Sepolia': {
-        'rpc_url': 'https://sepolia-rollup.arbitrum.io/rpc',
+        'rpc_url': 'https://endpoints.omniatech.io/v1/arbitrum/sepolia/public',
         'chain_id': 421614,
         'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
     },
     'OP Sepolia': {
-        'rpc_url': 'https://optimism-sepolia.blockpi.network/v1/rpc/public',
+        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
         'chain_id': 11155420,
         'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
     },
