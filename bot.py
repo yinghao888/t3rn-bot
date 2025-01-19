e1498acf93acd6c954c7fcffe5a97c8e559b4abd
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-01-19 16:36:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ def check_balance(web3, my_address):
 # 创建和发送交易的函数
 def send_bridge_transaction(web3, account, my_address, data, network_name):
     nonce = web3.eth.get_transaction_count(my_address, 'pending')
-    value_in_ether = 0.1
+    value_in_ether = 0.4
     value_in_wei = web3.to_wei(value_in_ether, 'ether')
 
     try:
@@ -198,9 +198,9 @@ def main():
         my_address = Account.from_key(private_keys[0]).address  # 使用第一个私钥的地址
         balance = check_balance(web3, my_address)
 
-        # 如果余额不足 0.1 ETH，切换到另一个链
-        if balance < 0.1:
-            print(f"{chain_symbols[current_network]}{current_network}余额不足 0.1 ETH，切换到 {alternate_network}{reset_color}")
+        # 如果余额不足 0.4 ETH，切换到另一个链
+        if balance < 0.4:
+            print(f"{chain_symbols[current_network]}{current_network}余额不足 0.4 ETH，切换到 {alternate_network}{reset_color}")
             current_network, alternate_network = alternate_network, current_network  # 交换链
 
         # 处理当前链的交易
