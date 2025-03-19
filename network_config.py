3165d6ffc28095c12059367ef3d55150528126f9
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-03-19 19:53:33 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -5,11 +5,11 @@ networks = {
         'rpc_url': 'https://sepolia.base.org', 
         'chain_id': 84532, 
         'contract_address': 
-        '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5' 
+        '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
     },
     'OP Sepolia': {
         'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
         'chain_id': 11155420,
-        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
+        'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
 }
