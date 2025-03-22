e989e9c622f82939f5752f4c03536c809cb464d9
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-03-22 09:44:31 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -3,7 +3,7 @@
 networks = {
     'Arbitrum': {
         'rpc_url': 'https://sepolia.arbitrum.io', 
-        'chain_id': 421613,  
+        'chain_id': 421614,  
         'contract_address': 
         '0x22B65d0B9b59af4D3Ed59F18b9Ad53f5F4908B54'  
     },
