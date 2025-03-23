0a4644c680c5cc14c4dec7763c6db51cb84a2546
nhxao <156049291+yinghao888@users.noreply.github.com>
2025-03-23 13:36:22 +0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,13 +2,13 @@
 
 networks = {
     'Arbitrum': {
-        'rpc_url': 'https://sepolia.arbitrum.io', 
+        'rpc_url': 'https://go.getblock.io/868382d71fcc4169bf248df24ffbfb83', 
         'chain_id': 421614,  
         'contract_address': 
         '0x22B65d0B9b59af4D3Ed59F18b9Ad53f5F4908B54'  
     },
     'OP Sepolia': {
-        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
+        'rpc_url': 'https://go.getblock.io/6a87203a95204612ad1a8e38e182055b',
         'chain_id': 11155420,
         'contract_address': 
         '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
